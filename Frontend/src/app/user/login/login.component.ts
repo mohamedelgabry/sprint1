05a238bfd32636c389c9ceea3b15cfaf06c2488f
Karim ElGhandour <kghandour@live.com>
2018-02-28 10:35:32 +0200

    Fixes to frontend

--- Frontend/src/app/user/login/login.component.ts
+++ Frontend/src/app/user/login/login.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   template: `
-  <div class="form-group>
+
   <p>Username</p>
   <input type="text" class="form-control" ng-model="username" placeholder="username" ng-model="username" required=""/><br>
   <p>Password</p>
   <input type="password" class="form-control" ng-model="password" placeholder="password" ng-model="password" required=""/><br>
   <button type="submit" class="btn btn-success" ng-click="submit()">Login</button>
-  </div>
-{{username}}`
+  {{ usernameset }}
+`
 })
 export class LoginComponent {
 
@@ -20,7 +20,7 @@ route($routeProvider)
 };
 
 submit($scope){
-  $scope.username
+  console.log($scope.username);
 }
 
 submitMethod($scope,$location)
