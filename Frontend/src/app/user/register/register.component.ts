f77ad4421ba8ecc03b314124ed59ee5876403463
Loai Alaa <loai.alaa7@gmail.com>
2018-02-28 22:43:40 +0200

    Added the register page

--- Frontend/src/app/user/register/register.component.ts
+++ Frontend/src/app/user/register/register.component.ts
@@ -1,7 +1,71 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
-  template: `REGISTER PAGE HERE WRITE HTML DIRECTLY`
+  template: `\`
+
+  <div class="col-md col-md-offset-2" (ngSubmit)="onSubmit()">
+      <form [formGroup]="myForm">
+          <div class="form-group">
+              <div>
+                  <label for="uname">Username :</label>
+                  <input type="text" id="uname" class="form-control"
+                         formControlName="userName"></div>
+
+              
+              <div>
+                  <label for="fname">First Name :</label>
+                  <input type="text" id="fname" class="form-control"
+                         formControlName="firstName"></div>
+
+              <div>
+                  <label for="lname">Last Name :</label>
+                  <input type="text" id="lname" class="form-control"
+                         formControlName="lastName">
+              </div>
+
+              <div>
+                  <label for="pass">Password :</label>
+                  <input type="password" id="pass" class="form-control"
+                         formControlName="password">
+              </div>
+
+              <div>
+                  <label for="email">Email :</label>
+                  <input type="text" id="email" class="form-control"
+                         formControlName="email">
+              </div>
+
+          </div>
+          <button class="btn btn-primary" [disabled]="!myForm.valid" type="submit">Sign me up!</button>
+      </form>
+  </div>
+  `
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit{
+
+
+    myForm: FormGroup;
+
+    onSubmit(){
+
+        console.log("hi");
+        this.myForm.reset();
+
+
+    }
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            userName: new FormControl(null, Validators.required),
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]),
+            password: new FormControl(null, Validators.required)
+
+
+        });
+}}
--- Frontend/src/app/user/register/register.module.ts
+++ Frontend/src/app/user/register/register.module.ts
@@ -4,9 +4,10 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { RegisterRoutingModule } from './register-routing.module';
 
 import { RegisterComponent } from './register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  imports: [ThemeModule, RegisterRoutingModule],
+  imports: [ThemeModule, RegisterRoutingModule,FormsModule,ReactiveFormsModule],
   declarations: [RegisterComponent],
   providers: []
 })
