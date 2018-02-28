0aea59f8a56ca3766526907dfd322278e9cb73da
Karim ElGhandour <kghandour@live.com>
2018-02-28 10:32:34 +0200

    Front end testing

--- Frontend/src/app/user/login/login.component.ts
+++ Frontend/src/app/user/login/login.component.ts
@@ -2,6 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  template: `LOGIN STUFF HERE WRITE HTML DIRECTLY`
+  template: `
+  <div class="form-group>
+  <p>Username</p>
+  <input type="text" class="form-control" ng-model="username" placeholder="username" ng-model="username" required=""/><br>
+  <p>Password</p>
+  <input type="password" class="form-control" ng-model="password" placeholder="password" ng-model="password" required=""/><br>
+  <button type="submit" class="btn btn-success" ng-click="submit()">Login</button>
+  </div>
+{{username}}`
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+
+route($routeProvider)
+{
+};
+
+submit($scope){
+  $scope.username
+}
+
+submitMethod($scope,$location)
+{
+    $scope.submit=function()
+    {
+        var username=$scope.username;
+        var password=$scope.password;
+    };
+};
+
+}
